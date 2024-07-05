c82e607cd8b26a49c78dd56918ebf8450020090e
3bHussein <39864308+3bHussein@users.noreply.github.com>
2024-07-05 19:53:18 +0400

    /

--- first_try/Program.cs
+++ first_try/Program.cs
@@ -7,12 +7,24 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("hello from Only Write");
 
 
+// Revising
 int x=2;
-double d =5d;
+long l=22222343434;
+double d =5.54d;
 float f=3.2f;
+char C='H';
+string text="long word";
+bool Test =true;
 
 
-Console.WriteLine(f);
+string[] mylist =new string[2];
+
+mylist[0]="hello";
+
+
+// Console.WriteLine(Test.ToString());
+Console.WriteLine(mylist[0]);
+
 
 static int add(int x){
 return x+=1;

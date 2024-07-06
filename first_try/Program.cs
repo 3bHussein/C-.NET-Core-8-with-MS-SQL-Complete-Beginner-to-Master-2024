da6f3e40276fad1a881809857dea9c6eb35ec550
3bHussein <39864308+3bHussein@users.noreply.github.com>
2024-07-06 23:11:32 +0400

    /

--- first_try/Program.cs
+++ first_try/Program.cs
@@ -1,51 +1,62 @@
-﻿
-
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// Console.ReadLine();
-
-Console.WriteLine("hello from Only Write");
+﻿#region Works fine also my frist try to back to c#
+//do lots of stuff
 
 
-// Revising
-int x=2;
-long l=22222343434;
-double d =5.54d;
-float f=3.2f;
-char C='H';
-string text="long word";
-bool Test =true;
+// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// // Console.ReadLine();
+
+// Console.WriteLine("hello from Only Write");
 
 
-string[] mylist =new string[2];
-List<string> test= new List<string>() {"Milk","orage"};
-
-mylist[0]="hello";
-test.Add("egg");
-
-// Console.WriteLine(Test.ToString());
-Console.WriteLine(test[2]);
-
-// IEnumerable not indexing 
-IEnumerable<string> myGroceryIEnumerable = myGroceryList;
-
-// Console.WriteLine(myGroceryIEnumerable.First());
-
-// Multi array 
-string[,] myTwoDimensionalArray = new string[,] {
-    { "Apples", "Eggs" },
-    { "Milk", "Cheese" }
-};
-
-Console.WriteLine(myTwoDimensionalArray[1, 1]);
+// // Revising
+// int x=2;
+// long l=22222343434;
+// double d =5.54d;
+// float f=3.2f;
+// char C='H';
+// string text="long word";
+// bool Test =true;
 
 
+// string[] mylist =new string[2];
+// List<string> test= new List<string>() {"Milk","orage"};
 
-static int add(int x){
-return x+=1;
-}
+// mylist[0]="hello";
+// test.Add("egg");
+
+// // Console.WriteLine(Test.ToString());
+// Console.WriteLine(test[2]);
+
+// // IEnumerable not indexing 
+// IEnumerable<string> myGroceryIEnumerable = myGroceryList;
+
+// // Console.WriteLine(myGroceryIEnumerable.First());
+
+// // Multi array 
+// string[,] myTwoDimensionalArray = new string[,] {
+//     { "Apples", "Eggs" },
+//     { "Milk", "Cheese" }
+// };
+
+// Console.WriteLine(myTwoDimensionalArray[1, 1]);
 
 
-Console.WriteLine(add(5));
+
+// static int add(int x){
+// return x+=1;
+// }
 
 
+// Console.WriteLine(add(5));
+
+#endregion
+
+
+int myint=5;
+int myseconedint=10;
+
+
+
+myint ++ ;
+Console.WriteLine(myint);
